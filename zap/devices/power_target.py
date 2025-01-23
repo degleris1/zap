6672842e4dd8f5484c5e069355c74f968add2809
degleris1 <degleris1@gmail.com>
2025-01-23 11:17:42 -0800

    add l1 prox update

--- zap/devices/power_target.py
+++ zap/devices/power_target.py
@@ -86,18 +86,45 @@ class PowerTarget(AbstractDevice):
         target_power = self.parameterize(target_power=target_power)
         assert angle is None
 
-        return _admm_prox_update(power, rho_power, target_power)
+        if self.norm_order == 1:
+            return _admm_prox_update_l1(power, rho_power, target_power)
+        else:  # L2
+            return _admm_prox_update_l2(power, rho_power, target_power)
 
 
 @torch.jit.script
-def _admm_prox_update(power: list[torch.Tensor], rho: float, target_power: torch.Tensor):
+def _admm_prox_update_l2(power: list[torch.Tensor], rho: float, target_power: torch.Tensor):
     # Problem is
-    #     min_p    (1/2) * (p - p_target)^2 + (rho / 2) || (p - power) ||_2^2
+    #     min_p    (1/2) * || p - p_target ||_2^2 + (rho / 2) || (p - power) ||_2^2
     # Objective derivative is
     #    (p - p_target) +  rho (p - power) = 0
     # Which is solved by
     #     p = (p_target + rho * power) / (1 + rho)
 
     p = (target_power + rho * power[0]) / (1 + rho)
+
+    return [p], None
+
+
+@torch.jit.script
+def _admm_prox_update_l1(power: list[torch.Tensor], rho: float, target_power: torch.Tensor):
+    # Problem is
+    #     min_p    ||p - p_target ||_1 + (rho / 2) || (p - power) ||_2^2
+    # Objective subdifferential is
+    #    sign(p - p_target) +  rho (p - power) = 0
+    #    sign(p - p_target) + rho p = rho power
+    # Which is solved by
+    #     p = power - 1/rho    if power[0] > p_target + (1.0/rho)
+    #     p = power + 1/rho    if power[0] < p_target - (1.0/rho)
+    #     p = p_target         otherwise
+    # This is essentially the soft threshholding operator.
+    # See https://web.stanford.edu/~boyd/papers/pdf/prox_algs.pdf Section 6.5.2
+
+    if power[0] > target_power + (1.0 / rho):
+        p = power[0] - (1.0 / rho)
+    elif power[0] < target_power - (1.0 / rho):
+        p = power[0] + (1.0 / rho)
+    else:  # power[0] in [target_power - (1.0/rho), target_power + (1.0/rho)]
+        p = target_power
 
     return [p], None
