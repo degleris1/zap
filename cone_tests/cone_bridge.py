Fix broadcasting bug (#16)
* apply linter, move directory

* fix broadcasting bug

cone_tests/cone_bridge.py
@@ -1,48 +1,44 @@
 import marimo
 
-__generated_with = "0.11.21"
+__generated_with = "0.10.7"
 app = marimo.App(width="medium")
 
 
 @app.cell
-def _(__file__):
+def _():
     import marimo as mo
     import cvxpy as cp
     import numpy as np
     import scipy.sparse as sp
-    from cvxpy.reductions.dcp2cone.dcp2cone import Dcp2Cone
-    np.set_printoptions(formatter={'float': '{:6.3f}'.format})
-    import sys
-    import os
-    import scs
-    PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    sys.path.append(PROJECT_ROOT)
-    from zap.devices.conic.cone_bridge import ConeBridge
-    from zap.devices.conic.variable_device import VariableDevice
-    from zap.devices.conic.slack_device import SlackDevice
-    from zap.admm import ADMMSolver
     import torch
+    import scs
+
+    from cvxpy.reductions.dcp2cone.dcp2cone import Dcp2Cone
+    from zap.conic.cone_bridge import ConeBridge
+    from zap.conic.variable_device import VariableDevice
+    from zap.conic.slack_device import SlackDevice
+
+    from zap.admm import ADMMSolver
+
+    np.set_printoptions(formatter={'float': '{:6.3f}'.format})
     return (
         ADMMSolver,
         ConeBridge,
         Dcp2Cone,
-        PROJECT_ROOT,
         SlackDevice,
         VariableDevice,
         cp,
         mo,
         np,
-        os,
         scs,
         sp,
-        sys,
         torch,
     )
 
 
 @app.cell
 def _(cp, np, sp):
-    m, n = 2, 5 
+    m, n = 2, 5
     np.random.seed(42)
 
     # Create a random sparse matrix A
@@ -66,12 +62,30 @@ def _(cp, np, sp):
 
 
 @app.cell
+def _(A):
+    A.todense()
+    return
+
+
+@app.cell
+def _(b):
+    b
+    return
+
+
+@app.cell
+def _(cone_bridge):
+    cone_bridge.devices[2]
+    return
+
+
+@app.cell
 def _(s):
     s.value
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(cp, problem, scs):
     probdata, chain, inverse_data = problem.get_problem_data(cp.SCS)
 
@@ -98,7 +112,7 @@ def _(cp, problem, scs):
 
     print(f"A.shape: {cone_params['A'].shape}, c.shape: {cone_params['c'].shape}, b.shape: {cone_params['b'].shape}")
 
-    soln = scs.solve(data, cones)
+    soln = scs.solve(data, cones, verbose=False)
     return (
         chain,
         cone_dims,
@@ -118,6 +132,72 @@ def _(ConeBridge, cone_params):
 
 
 @app.cell
+def _(cone_bridge, cp):
+    ystar = cone_bridge.solve(cp.CLARABEL)
+    return (ystar,)
+
+
+@app.cell
+def _(ystar):
+    ystar.problem.value
+    return
+
+
+@app.cell
+def _(ystar):
+    ystar.problem.status
+    return
+
+
+@app.cell
+def _(ystar):
+    ystar.local_variables
+    return
+
+
+@app.cell
+def _(s, x):
+    x.value, s.value
+    return
+
+
+@app.cell
+def _(cone_params):
+    cone_params["A"].todense()
+    return
+
+
+@app.cell
+def _(cone_params):
+    cone_params["b"]
+    return
+
+
+@app.cell
+def _(cone_bridge):
+    cone_bridge.terminal_groups
+    return
+
+
+@app.cell
+def _(cone_bridge):
+    cone_bridge.device_group_map_list
+    return
+
+
+@app.cell
+def _(cone_bridge):
+    cone_bridge.devices[2]
+    return
+
+
+@app.cell
+def _(cone_params):
+    cone_params["b"]
+    return
+
+
+@app.cell
 def _(cone_bridge, torch):
     machine = "cpu"
     dtype = torch.float32
@@ -126,68 +206,54 @@ def _(cone_bridge, torch):
 
 
 @app.cell
-def _(ADMMSolver, admm_devices, cone_bridge, dtype, machine):
-    admm = ADMMSolver(
-        machine=machine,
-        dtype=dtype,
-        atol=1e-6,
-        rtol=1e-6,
-        track_objective=False,
-        rtol_dual_use_objective=False,
-        num_iterations = int(1e5),
-    )
+def _():
+    # admm = ADMMSolver(
+    #     machine=machine,
+    #     dtype=dtype,
+    #     atol=1e-6,
+    #     rtol=1e-6,
+    #     track_objective=False,
+    #     rtol_dual_use_objective=False,
+    #     num_iterations = int(1e5),
+    # )
 
-    solution_admm, history_admm = admm.solve(cone_bridge.net, admm_devices, cone_bridge.time_horizon)
-    return admm, history_admm, solution_admm
-
-
-@app.cell
-def _(VariableDevice, torch):
-    def get_solution(solution_admm, cone_bridge):
-        """
-        Parses out solution from the "flow" view into actual primal and slacks to compare against SCS
-        """
-        x = []
-        s = []
-        for idx, device in enumerate(cone_bridge.devices):
-            if type(device) is VariableDevice:
-                tensor_list = [t.squeeze() for t in solution_admm.power[idx]]
-                p_tensor = torch.stack(tensor_list, dim=0).flatten() 
-
-                A_v = torch.tensor(device.A_v, dtype=torch.float32)
-                A_expanded = torch.cat([torch.diag(A_v[i]) for i in range(A_v.shape[0])], dim=0)
-
-                x_recovered = torch.linalg.lstsq(A_expanded, p_tensor).solution
-
-                x.extend(x_recovered.squeeze())
-            
-
-            else:
-                cone_slacks = solution_admm.power[idx][0].flatten()
-                s.extend(cone_slacks + device.b_d.flatten())
-
-
-        return x, s
-            
-        
-    return (get_solution,)
-
-
-@app.cell
-def _(cone_bridge, get_solution, solution_admm):
-    x_admm, s_admm = get_solution(solution_admm, cone_bridge)
-    return s_admm, x_admm
-
-
-@app.cell
-def _(x_admm):
-    x_admm
+    # solution_admm, history_admm = admm.solve(cone_bridge.net, admm_devices, cone_bridge.time_horizon)
     return
 
 
 @app.cell
-def _(s_admm):
-    s_admm
+def _():
+    # def get_solution(solution_admm, cone_bridge):
+    #     """
+    #     Parses out solution from the "flow" view into actual primal and slacks to compare against SCS
+    #     """
+    #     x = []
+    #     s = []
+    #     for idx, device in enumerate(cone_bridge.devices):
+    #         if type(device) is VariableDevice:
+    #             tensor_list = [t.squeeze() for t in solution_admm.power[idx]]
+    #             p_tensor = torch.stack(tensor_list, dim=0).flatten() 
+
+    #             A_v = torch.tensor(device.A_v, dtype=torch.float32)
+    #             A_expanded = torch.cat([torch.diag(A_v[i]) for i in range(A_v.shape[0])], dim=0)
+
+    #             x_recovered = torch.linalg.lstsq(A_expanded, p_tensor).solution
+
+    #             x.extend(x_recovered.squeeze())
+
+
+    #         else:
+    #             cone_slacks = solution_admm.power[idx][0].flatten()
+    #             s.extend(cone_slacks + device.b_d.flatten())
+
+
+    #     return x, s
+    return
+
+
+@app.cell
+def _():
+    # x_admm, s_admm = get_solution(solution_admm, cone_bridge)
     return
 
 

zap/conic/cone_bridge.py
@@ -1,7 +1,9 @@
-import zap.network
-from zap.devices.conic.variable_device import VariableDevice
-from zap.devices.conic.slack_device import ZeroConeSlackDevice, NonNegativeConeSlackDevice
 import numpy as np
+import cvxpy as cp
+
+from zap.network import PowerNetwork
+from .variable_device import VariableDevice
+from .slack_device import ZeroConeSlackDevice, NonNegativeConeSlackDevice
 from scipy.sparse import csc_matrix, isspmatrix_csc
 
 
@@ -27,7 +29,7 @@ class ConeBridge:
         self._create_slack_devices()
 
     def _build_network(self):
-        self.net = zap.network.PowerNetwork(self.A.shape[0])
+        self.net = PowerNetwork(self.A.shape[0])
 
     def _group_variable_devices(self):
         """
@@ -41,20 +43,26 @@ class ConeBridge:
         self.terminal_groups = np.sort(np.unique(num_terminals_per_device_list))
 
         # List of lists—each sublist contains the indices of devices with the same number of terminals
-        self.device_group_map_list = [np.argwhere(num_terminals_per_device_list == g).flatten() for g in self.terminal_groups]
-
+        self.device_group_map_list = [
+            np.argwhere(num_terminals_per_device_list == g).flatten()
+            for g in self.terminal_groups
+        ]
 
     def _create_variable_devices(self):
         for group_idx, num_terminals_per_device in enumerate(self.terminal_groups):
+            # Retrieve relevant columsn of A
             device_idxs = self.device_group_map_list[group_idx]
             num_devices = len(device_idxs)
+
             A_devices = self.A[:, device_idxs]
 
             # (i) A_v is a submatrix of A: (num_terminals, num_devices)
-            A_v = A_devices.data.reshape((num_devices, num_terminals_per_device)).T  
+            A_v = A_devices.data.reshape((num_devices, num_terminals_per_device)).T
 
             # (ii) terminal_device_array: (num_devices, num_terminals_per_device)
-            terminal_device_array = A_devices.indices.reshape((num_devices, num_terminals_per_device))
+            terminal_device_array = A_devices.indices.reshape(
+                (num_devices, num_terminals_per_device)
+            )
 
             # (iii) cost vector (subvector of c taking the corresponding device elements)
             cost_vector = self.c[device_idxs]
@@ -69,17 +77,20 @@ class ConeBridge:
 
     def _group_slack_devices(self):
         """
-        Currently assuming all zero cones before non-negative cones in CVXPY 
+        Currently assuming all zero cones before non-negative cones in CVXPY
         """
 
         num_zero_cone = self.K["z"]
-        num_nonneg_cone = self.K["l"]
+        # num_nonneg_cone = self.K["l"]
         slack_indices = np.arange(self.b.shape[0])
 
-        self.zero_cone_slacks = list(zip(slack_indices[:num_zero_cone],self.b[:num_zero_cone]))
-        self.nonneg_cone_slacks = list(zip(slack_indices[num_zero_cone:],self.b[num_zero_cone:]))
+        self.zero_cone_slacks = list(
+            zip(slack_indices[:num_zero_cone], self.b[:num_zero_cone])
+        )
+        self.nonneg_cone_slacks = list(
+            zip(slack_indices[num_zero_cone:], self.b[num_zero_cone:])
+        )
 
-    
     def _create_slack_devices(self):
         if self.zero_cone_slacks:
             terminals, b_d_values = zip(*self.zero_cone_slacks)
@@ -99,3 +110,7 @@ class ConeBridge:
             )
             self.devices.append(nonneg_cone_device)
 
+    def solve(self, solver=cp.CLARABEL, **kwargs):
+        return self.net.dispatch(
+            self.devices, self.time_horizon, add_ground=False, solver=solver, **kwargs
+        )