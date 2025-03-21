fix broadcasting bug

cone_tests/cone_bridge.py
@@ -38,7 +38,7 @@ def _():
 
 @app.cell
 def _(cp, np, sp):
-    m, n = 2, 5 
+    m, n = 2, 5
     np.random.seed(42)
 
     # Create a random sparse matrix A
@@ -62,12 +62,30 @@ def _(cp, np, sp):
 
 
 @app.cell
+def _(A):
+    A.todense()
+    return
+
+
+@app.cell
+def _(b):
+    b
+    return
+
+
+@app.cell
+def _(cone_bridge):
+    cone_bridge.devices[2]
+    return
+
+
+@app.cell
 def _(s):
     s.value
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(cp, problem, scs):
     probdata, chain, inverse_data = problem.get_problem_data(cp.SCS)
 
@@ -94,7 +112,7 @@ def _(cp, problem, scs):
 
     print(f"A.shape: {cone_params['A'].shape}, c.shape: {cone_params['c'].shape}, b.shape: {cone_params['b'].shape}")
 
-    soln = scs.solve(data, cones)
+    soln = scs.solve(data, cones, verbose=False)
     return (
         chain,
         cone_dims,
@@ -114,14 +132,44 @@ def _(ConeBridge, cone_params):
 
 
 @app.cell
-def _(cone_params):
-    cone_params["A"].shape
+def _(cone_bridge, cp):
+    ystar = cone_bridge.solve(cp.CLARABEL)
+    return (ystar,)
+
+
+@app.cell
+def _(ystar):
+    ystar.problem.value
     return
 
 
 @app.cell
-def _(cones):
-    cones
+def _(ystar):
+    ystar.problem.status
+    return
+
+
+@app.cell
+def _(ystar):
+    ystar.local_variables
+    return
+
+
+@app.cell
+def _(s, x):
+    x.value, s.value
+    return
+
+
+@app.cell
+def _(cone_params):
+    cone_params["A"].todense()
+    return
+
+
+@app.cell
+def _(cone_params):
+    cone_params["b"]
     return
 
 
@@ -139,7 +187,7 @@ def _(cone_bridge):
 
 @app.cell
 def _(cone_bridge):
-    cone_bridge.devices[3]
+    cone_bridge.devices[2]
     return
 
 

zap/conic/cone_bridge.py
@@ -1,10 +1,9 @@
-import zap.network
-from .variable_device import VariableDevice
-from .slack_device import (
-    ZeroConeSlackDevice,
-    NonNegativeConeSlackDevice,
-)
 import numpy as np
+import cvxpy as cp
+
+from zap.network import PowerNetwork
+from .variable_device import VariableDevice
+from .slack_device import ZeroConeSlackDevice, NonNegativeConeSlackDevice
 from scipy.sparse import csc_matrix, isspmatrix_csc
 
 
@@ -30,7 +29,7 @@ class ConeBridge:
         self._create_slack_devices()
 
     def _build_network(self):
-        self.net = zap.network.PowerNetwork(self.A.shape[0])
+        self.net = PowerNetwork(self.A.shape[0])
 
     def _group_variable_devices(self):
         """
@@ -110,3 +109,8 @@ class ConeBridge:
                 b_d=np.array(b_d_values),
             )
             self.devices.append(nonneg_cone_device)
+
+    def solve(self, solver=cp.CLARABEL, **kwargs):
+        return self.net.dispatch(
+            self.devices, self.time_horizon, add_ground=False, solver=solver, **kwargs
+        )