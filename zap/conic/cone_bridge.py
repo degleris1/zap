f402832a54688a657d2e7dd58b214e82c38f378b
asreek6654 <akshay81@stanford.edu>
2025-04-23 09:06:23 -0700

    added binning for soc devices + tested

--- zap/conic/cone_bridge.py
+++ zap/conic/cone_bridge.py
@@ -12,7 +12,7 @@ from scipy.sparse import csc_matrix, isspmatrix_csc
 
 
 class ConeBridge:
-    def __init__(self, cone_params: dict):
+    def __init__(self, cone_params: dict, grouping_params: dict | None = None):
         self.A = cone_params["A"]
         self.b = cone_params["b"]
         self.c = cone_params["c"]
@@ -23,11 +23,25 @@ class ConeBridge:
 
         if not isspmatrix_csc(self.A):
             self.A = csc_matrix(self.A)
+
+        grouping_params = grouping_params or {}
+        self.variable_grouping_strategy = grouping_params.get(
+            "variable_grouping_strategy", "binned_terminal_groups"
+        )
+        self.slack_grouping_strategy = grouping_params.get(
+            "slack_grouping_strategy", "binned_soc_groups"
+        )
+        self.variable_grouping_bin_edges = grouping_params.get(
+            "variable_grouping_bin_edges", (0, 10, 100, 1000)
+        )
+        self.slack_grouping_bin_edges = grouping_params.get(
+            "slack_grouping_bin_edges", (0, 10, 100, 1000)
+        )
         self._transform()
 
     def _transform(self):
         self._build_network()
-        self._group_variable_devices(strategy="binned_terminal_groups")
+        self._group_variable_devices()
         self._create_variable_devices()
         self._group_slack_devices()
         self._create_slack_devices()
@@ -35,18 +49,16 @@ class ConeBridge:
     def _build_network(self):
         self.net = PowerNetwork(self.A.shape[0])
 
-    def _group_variable_devices(
-        self, strategy="binned_terminal_groups", bin_edges=(0, 10, 100, 1000)
-    ):
+    def _group_variable_devices(self):
         """
         Figure out the appropriate grouping of variable devices based on the number of terminals they have.
         """
-        if strategy == "discrete_terminal_groups":
+        if self.variable_grouping_strategy == "discrete_terminal_groups":
             # Each group consists of devices with exactly the same number of terminals
             self._compute_discrete_terminal_groups()
-        elif strategy == "binned_terminal_groups":
+        elif self.variable_grouping_strategy == "binned_terminal_groups":
             # Each group consists of devices with a number of terminals in the same bin
-            self._compute_binned_terminal_groups(bin_edges)
+            self._compute_binned_terminal_groups(self.variable_grouping_bin_edges)
 
     def _compute_binned_terminal_groups(self, bin_edges):
         """
@@ -58,7 +70,6 @@ class ConeBridge:
         num_terminals_per_device_list = np.diff(self.A.indptr)
         positive_mask = num_terminals_per_device_list > 0
         filtered_counts = num_terminals_per_device_list[positive_mask]
-        # counts = num_terminals_per_device_list[positive_mask]
         device_idxs = np.nonzero(positive_mask)[0]
 
         # Account for upper bound bin (last bin takes everything else)
@@ -137,9 +148,6 @@ class ConeBridge:
 
         num_zero_cone = self.K["z"]
         num_nonneg_cone = self.K["l"]
-        # This is like self.terminal_groups for variable devices
-        self.soc_terminal_groups = np.sort(np.unique(self.K["q"]))
-        self.soc_blocks = self.K["q"]
         self.slack_indices = np.arange(self.b.shape[0])
 
         # Group zero cone slacks
@@ -155,16 +163,66 @@ class ConeBridge:
         )
 
         # Group SOC cone slacks
-        # We are creating a dict like {block_size: [(start, end), ...]}
-        # where each entry corresponds to a block of SOC slacks,
-        # and each tuple in the list is for a block (device) of that size
+        if self.slack_grouping_strategy == "discrete_soc_groups":
+            self._compute_discrete_soc_groups(soc_start=end_nonneg)
+        elif self.slack_grouping_strategy == "binned_soc_groups":
+            self._compute_binned_soc_groups(
+                bin_edges=self.slack_grouping_bin_edges, soc_start=end_nonneg
+            )
+
+    def _compute_binned_soc_groups(self, bin_edges, soc_start):
+        """
+        This function makes a separate group for each set of SOC slacks in the same bin.
+        """
+        self.soc_blocks = self.K["q"]
+        start_idx = soc_start
+        # Account for upper bound bin (last bin takes everything else)
+        edges = np.asarray(bin_edges, dtype=np.int64)
+        edges = np.concatenate(
+            [edges, [np.iinfo(np.int64).max]]
+        )  # Do this instead of np.inf to keep it in int
+
+        soc_bins = [[] for _ in range(len(edges) - 1)]
+
+        # Loop through the blocks of SOC slacks
+        # and assign them to the appropriate bin
+        # We are interested in saving the start and end indices of the blocks,
+        # as well as the size of the block
+        # (which is the number of terminals in the block)
+        for k in self.soc_blocks:
+            start, end = start_idx, start_idx + k
+
+            # Bin assignment
+            bin_idx = np.digitize(k, edges, right=True) - 1
+            soc_bins[bin_idx].append((start, end, k))
+            start_idx = end
+
+        # Remove empty bins
+        self.soc_device_group_map_list = [bin_block for bin_block in soc_bins if bin_block]
+
+    def _compute_discrete_soc_groups(self, soc_start):
+        """
+        This function makes a separate device for each block of SOC slacks.
+        We are creating a dict like {block_size: [(start, end), ...]}
+        where each entry corresponds to a block of SOC slacks,
+        and each tuple in the list is for a block (device) of that size.
+        We convert this into the list of lists of tuples for compatability
+        with the binning approach.
+        """
+        self.soc_device_group_map_list = []
+
+        # This is like self.terminal_groups for variable devices
+        self.soc_terminal_groups = np.sort(np.unique(self.K["q"]))
+        self.soc_blocks = self.K["q"]
         self.soc_block_idxs_dict = {group_size: [] for group_size in self.soc_terminal_groups}
-        soc_start = end_nonneg
         for block_size in self.soc_blocks:
             start = soc_start
             end = soc_start + block_size
-            self.soc_block_idxs_dict[block_size].append((start, end))
+            self.soc_block_idxs_dict[block_size].append((start, end, block_size))
             soc_start += block_size
+        self.soc_device_group_map_list = [
+            self.soc_block_idxs_dict[block_size] for block_size in sorted(self.soc_block_idxs_dict)
+        ]
 
     def _create_slack_devices(self):
         if self.zero_cone_slacks:
@@ -186,16 +244,21 @@ class ConeBridge:
             self.devices.append(nonneg_cone_device)
 
         # Create SOC devices
-        for group_idx, num_terminals_per_device in enumerate(self.soc_terminal_groups):
-            group_slices = self.soc_block_idxs_dict[num_terminals_per_device]
-            b_d_array = np.column_stack([self.b[start:end] for (start, end) in group_slices])
-            terminal_device_array = np.row_stack(
-                [self.slack_indices[start:end] for (start, end) in group_slices]
-            )
+        for bin_blocks in self.soc_device_group_map_list:
+            k_max = max(k for _, _, k in bin_blocks)
+            num_devices = len(bin_blocks)
+
+            b_d_array = np.zeros((k_max, num_devices), dtype=self.b.dtype)
+            terminals = -np.ones((num_devices, k_max), dtype=np.int64)
+            for bin_idx, (start, end, k) in enumerate(bin_blocks):
+                b_d_array[:k, bin_idx] = self.b[start:end]
+                terminals[bin_idx, :k] = self.slack_indices[start:end]
+
             soc_cone_device = SecondOrderConeSlackDevice(
                 num_nodes=self.net.num_nodes,
-                terminals=terminal_device_array,
+                terminals=terminals,
                 b_d=b_d_array,
+                terminals_per_device=np.array([k for _, _, k in bin_blocks]),
             )
             self.devices.append(soc_cone_device)
 
